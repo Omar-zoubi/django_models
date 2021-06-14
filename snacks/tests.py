a33740b9c5a4e701913ce9ca96e7b5fd1a17fdc8
Omar-zoubi <o.fandi02@gmail.com>
2021-06-14 21:43:02 +0300

    sss

--- snacks/tests.py
+++ snacks/tests.py
@@ -1,20 +1,27 @@
 from django.test import TestCase
 from django.urls import reverse
+from .views import snacksListView, snackDetailView
 
-class snackstest(TestCase):
+# Create your tests here.
+
+class snackTests(TestCase):
     def test_home(self):
         url = reverse('list')
         response = self.client.get(url)
-        self.assertEqual(response.status_code,200)
+        self.assertEqual(response.status_code, 200)
 
-    def test_templete(self):
+    def test_template(self):
         url = reverse('list')
         response = self.client.get(url)
         self.assertTemplateUsed(response, 'list.html')
         self.assertTemplateUsed(response, 'base.html')
 
-    def test_templete_tow(self):
-        url = reverse('details')
+    def test_details_code(self):
+        url = reverse('details', args="1")
         response = self.client.get(url)
-        self.assertTemplateUsed(response, 'details.html')
-        self.assertTemplateUsed(response, 'base.html')
+        self.assertEqual(response.status_code, 404)
+
+    def test_details_template(self):
+        url = reverse('details', args="1")
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 404)
